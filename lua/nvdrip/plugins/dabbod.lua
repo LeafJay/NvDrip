22daaf629d1395a77b56e110d55d4b07d06394f4
LeafJay <leafjay001@gmail.com>
2023-11-17 17:16:03 +0100

    update paths

--- lua/nvdrip/plugins/dabbod.lua
+++ lua/nvdrip/plugins/dabbod.lua
@@ -12,7 +12,7 @@ return {
   },
   init = function()
     vim.g.db_ui_use_nerd_fonts = 1
-    vim.g.db_ui_save_location = "~/sources/sql"
+    vim.g.db_ui_save_location = "~/Sources/sql"
     vim.g.db_ui_auto_execute_table_helpers = 1
   end,
 }
